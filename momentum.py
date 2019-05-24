2fd92ce333356501ef7a6878121615fa688254ca
Daman <34317675+damansc@users.noreply.github.com>
2019-05-24 10:39:07 -0400

    Adding swing method

--- momentum.py
+++ momentum.py
@@ -40,3 +40,25 @@ df divergence(ticker='ticker', start=oneyr, plot=False, reg=False):
     condition = sorted_df > 0
     sorted_df['days-in-a-row'] = condition.cumsum() - condition.cumsum().where(~condition).ffill().astype(int)
     return sorted_df
+    
+    
+ ## make method that will be useful for analyze the current days
+ # pct change and compare that pct change to previous time
+ # periods of the similar change and analyze how they move
+ # the next few days afterwards as a way of making a swing trade.
+ 
+ def swing_or_nah(pctdelta=.05, ticker=None, days=1):
+ 	if ticker==None:
+ 		ticker_list = []
+ 		for x in df.columns.values:
+ 			if df[x][df.datetime.now().date():].pct_change() >= 0.05:
+ 				 ticker_list.append(x)
+ 				 print('Stocks with {} percent change: {}'.format(pctdelta, ticker_list))
+ 		if ticker != None:
+ 			df = sp500_meta[ticker]['Adj Close'].pct_change()
+ 			condition_dates = df[(df >= 0.05)].index.values
+ 			condition_post = condition_dates+dt.timedelta(days)
+ 			df_post = df[df.index == condition_post]
+ 			avg_post = df_post.describe()
+ 			return(avg_post)
+ 			
